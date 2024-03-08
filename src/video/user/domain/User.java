ae2427002426529166113697aa906377edc1845e
SJHlll <ssaadd1212@gmail.com>
2024-03-08 12:26:26 +0900

    # Java Basic(DVD 관리 프로그램 OrderService 반납 기능 완료)

--- src/video/user/domain/User.java
+++ src/video/user/domain/User.java
@@ -7,6 +7,11 @@ import java.util.Map;
 
 public class User {
 
+    public static final int SILVER_PRICE = 5000;
+    public static final int GOLD_PRICE = 7000;
+    public static final int VIP_PRICE = 10000;
+
+
     private static int sequence; // 회원 누적 순차번호
 
     private int userNumber;
@@ -14,10 +19,13 @@ public class User {
     private String phoneNumber;
     private Grade grade;
     private int totalPaying; // 총 결제 금액
+
     private Map<Integer, Order> orderList; // 각 회원마다 가지는 대여 목록
 
+
+
     public User(String userName, String phoneNumber, Grade grade) {
-        this.userNumber = ++sequence; // 회원이 생성될 때 회원 고유 번호를 하나 올려서 대입
+        this.userNumber = ++sequence; // 회원이 생성될 때 회원 고유 번호를 하나 올려서 대입.
         this.userName = userName;
         this.phoneNumber = phoneNumber;
         this.grade = grade;
@@ -79,6 +87,17 @@ public class User {
 
     public void setTotalPaying(int totalPaying) {
         this.totalPaying += totalPaying;
+
+        // 회원이 대여를 진행할 때 이 setter가 호출됨.
+        // 총 결제금액에 따라 회원 등급을 조정.
+        if (this.totalPaying >= VIP_PRICE) {
+            this.grade = Grade.VIP;
+        } else if (this.totalPaying >= GOLD_PRICE) {
+            this.grade = Grade.GOLD;
+        } else if (this.totalPaying >= SILVER_PRICE) {
+            this.grade = Grade.SILVER;
+        }
+
     }
 
     @Override
@@ -86,7 +105,6 @@ public class User {
         return  "## 회원번호: " + userNumber +
                 ", 회원명: " + userName +
                 ", 전화번호: " + phoneNumber  +
-//                ", 총 결제금액: " + totalPaying + "원" +
                 ", 등급: " + grade;
     }
 }
